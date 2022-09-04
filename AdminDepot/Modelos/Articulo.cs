3d26586b181c9a93975c11e77daf91cfb35b28d9
RoberRang <rob_rang@hotmail.com>
2022-09-04 20:00:39 -0300

    Se agrega las tablas para la aplicacion de catalogo

---
+++ AdminDepot/Modelos/Articulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminDepot.Modelos
+{
+    internal class Articulo
+    {
+        
+
+    }
+}
