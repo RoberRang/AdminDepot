e926b16ee385538dac87cd8bc0f547ae1a1ded4b
RoberRang <rob_rang@hotmail.com>
2022-09-04 20:07:03 -0300

    Se agrega al modelo Articulo, Categoria y Marca

--- AdminDepot/Modelos/Articulo.cs
+++ AdminDepot/Modelos/Articulo.cs
@@ -10,7 +10,14 @@ namespace AdminDepot.Modelos
 {
     internal class Articulo
     {
-        
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Nullable<int> IdMarca { get; set; }
+        public Nullable<int> IdCategoria { get; set; }
+        public string ImagenUrl { get; set; }
+        public Nullable<decimal> Precio { get; set; }
 
     }
 }
